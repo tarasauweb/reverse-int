973b8ce615828cadb44bafdd5705ceb74bdcb134
Artur Tarasau <tarasov.a.web@gmail.com>
2023-01-04 16:41:46 +0300

    complite task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let myNum = String(n).split('')
+    myNum = myNum.filter(item=>{
+        if(item !== '-') {
+            return item
+        }
+    }).reverse().join('')
+    return Number(myNum)
 }
